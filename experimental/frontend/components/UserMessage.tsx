c14ff1dc447f484996fed26f1942468e47237b1b
Elephant200 <alexktev@gmail.com>
2025-03-23 17:41:09 -0500

    Move experimental directory out of project

--- experimental/frontend/components/UserMessage.tsx
+++
@@ -1,8 +0,0 @@
-interface UserMessageProps {
-    text: string;
-  }
-  
-  export default function UserMessage({ text }: UserMessageProps) {
-    return <div className="user-message">{text}</div>;
-  }
-  
