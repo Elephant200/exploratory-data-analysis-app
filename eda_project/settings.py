48e99572adfa2f0bcc31ce5549db02cc00dd1900
Alex <alexktev@gmail.com>
2025-04-11 14:10:56 +0000

    Add replit dev domain to trusted CSRF origins

--- eda_project/settings.py
+++ eda_project/settings.py
@@ -14,6 +14,10 @@ ALLOWED_HOSTS = [
     "c48a7eb7-b62d-470d-a224-9b6c7c66cdcd-00-1w2l6xjyoka2f.worf.replit.dev"
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://c48a7eb7-b62d-470d-a224-9b6c7c66cdcd-00-1w2l6xjyoka2f.worf.replit.dev"
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
