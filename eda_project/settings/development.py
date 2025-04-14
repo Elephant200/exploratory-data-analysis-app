3d41bfffd9303dd79a28750c99e12d829dcb22f8
Elephant200 <alexktev@gmail.com>
2025-04-14 14:26:54 -0400

    Add 127.0.0.1 and localhost to allowed dev hosts

--- eda_project/settings/development.py
+++ eda_project/settings/development.py
@@ -3,9 +3,13 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = [
-		"c48a7eb7-b62d-470d-a224-9b6c7c66cdcd-00-1w2l6xjyoka2f.worf.replit.dev"
+		"c48a7eb7-b62d-470d-a224-9b6c7c66cdcd-00-1w2l6xjyoka2f.worf.replit.dev",
+        "127.0.0.1",
+        "localhost"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-		"https://c48a7eb7-b62d-470d-a224-9b6c7c66cdcd-00-1w2l6xjyoka2f.worf.replit.dev"
+		"https://c48a7eb7-b62d-470d-a224-9b6c7c66cdcd-00-1w2l6xjyoka2f.worf.replit.dev",
+        "127.0.0.1",
+        "localhost"
 ]
