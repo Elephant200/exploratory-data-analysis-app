5fd5fa19e938282e687b4273d6976154dd5d73b9
Alex <alexktev@gmail.com>
2025-04-14 20:16:31 +0000

    Add 127.0.0.1 to production hosts

--- eda_project/settings/production.py
+++ eda_project/settings/production.py
@@ -2,6 +2,6 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["exploratory-data-analysis.replit.app"]
+ALLOWED_HOSTS = ["exploratory-data-analysis.replit.app", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ["https://exploratory-data-analysis.replit.app"]
