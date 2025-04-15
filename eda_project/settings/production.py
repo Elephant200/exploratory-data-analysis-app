496c27707295922f4de2cbe72e95b6e87c27cbff
Elephant200 <alexktev@gmail.com>
2025-04-15 11:42:24 -0400

    Add WhiteNoise middleware

--- eda_project/settings/production.py
+++ eda_project/settings/production.py
@@ -5,3 +5,6 @@ DEBUG = False
 ALLOWED_HOSTS = ["exploratory-data-analysis.replit.app", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = ["https://exploratory-data-analysis.replit.app", "http://127.0.0.1"]
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
